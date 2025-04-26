3469183343e574ed483eacd9ef264503357d333c
mdmonjumon <mdmonjumon786@gmail.com>
2025-04-27 00:36:05 +0600

    environmental variables created

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBZCwsPO1FqnSbayoIvHiKHVeBhev02Y4Y",
-  authDomain: "winter-clothing-donation-2d550.firebaseapp.com",
-  projectId: "winter-clothing-donation-2d550",
-  storageBucket: "winter-clothing-donation-2d550.firebasestorage.app",
-  messagingSenderId: "495599910771",
-  appId: "1:495599910771:web:89532db6b1fdd5b1a2cf06"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
